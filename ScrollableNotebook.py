b9701d3dc0f6a7f382df4d5c84d076ea3a42ad83
Muhammet Emin TURGUT <160205017@kku.edu.tr>
2020-02-10 02:10:06 +0300

    Added All Notebook Functions

--- ScrollableNotebook.py
+++ ScrollableNotebook.py
@@ -13,32 +13,32 @@ class ScrollableNotebook(ttk.Frame):
         self.notebookContent.pack(fill="both", expand=True)
 
         self.notebookTab = ttk.Notebook(self,**kwargs)
-        self.notebookTab.bind("<<NotebookTabChanged>>",self.tabChanger)
+        self.notebookTab.bind("<<NotebookTabChanged>>",self._tabChanger)
 
         slideFrame = ttk.Frame(self)
         slideFrame.place(relx=1.0, x=0, y=1, anchor=NE)
         leftArrow = ttk.Label(slideFrame, text="\u25c0")
-        leftArrow.bind("<1>",self.leftSlide)
+        leftArrow.bind("<1>",self._leftSlide)
         leftArrow.pack(side=LEFT)
         rightArrow = ttk.Label(slideFrame, text=" \u25b6")
-        rightArrow.bind("<1>",self.rightSlide)
+        rightArrow.bind("<1>",self._rightSlide)
         rightArrow.pack(side=RIGHT)
-        self.notebookContent.bind( "<Configure>", self.resetSlide)
+        self.notebookContent.bind( "<Configure>", self._resetSlide)
 
-    def tabChanger(self,event):
+    def _tabChanger(self,event):
         self.notebookContent.select(self.notebookTab.index("current"))
         
-    def rightSlide(self,event):
+    def _rightSlide(self,event):
         if self.notebookTab.winfo_width()>self.notebookContent.winfo_width()-30:
             if (self.notebookContent.winfo_width()-(self.notebookTab.winfo_width()+self.notebookTab.winfo_x()))<=35:
                 self.xLocation-=20
                 self.notebookTab.place(x=self.xLocation,y=0)
-    def leftSlide(self,event):
+    def _leftSlide(self,event):
         if not self.notebookTab.winfo_x()== 0:
             self.xLocation+=20
             self.notebookTab.place(x=self.xLocation,y=0)
 
-    def resetSlide(self,event):
+    def _resetSlide(self,event):
         self.notebookTab.place(x=0,y=0)
         self.xLocation = 0
 
@@ -48,3 +48,35 @@ class ScrollableNotebook(ttk.Frame):
         else:
             self.notebookContent.add(frame, text="")
         self.notebookTab.add(ttk.Frame(self.notebookTab),**kwargs)
+
+    def forget(self,tab_id):
+        self.notebookContent.forget(tab_id)
+        self.notebookTab.forget(tab_id)
+
+    def hide(self,tab_id):
+        self.notebookContent.hide(tab_id)
+        self.notebookTab.hide(tab_id)
+
+    def identify(self,x, y):
+        return self.notebookTab.identify(x,y)
+
+    def index(self,tab_id):
+        return self.notebookTab.index(tab_id)
+
+    def insert(self,pos,frame, **kwargs):
+        self.notebookContent.insert(pos,frame, **kwargs)
+        self.notebookTab.insert(pos,frame,**kwargs)
+
+    def select(self,tab_id):
+        self.notebookContent.select(tab_id)
+        self.notebookTab.select(tab_id)
+
+    def tab(self,tab_id, option=None, **kwargs):
+        return self.notebookTab.tab(tab_id, option=None, **kwargs)
+
+    def tabs(self):
+        return self.notebookContent.tabs()
+
+    def enable_traversal(self):
+        self.notebookContent.enable_traversal()
+        self.notebookTab.enable_traversal()
